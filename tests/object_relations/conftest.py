bfdf630da2eaeafc07e2f16354cfa46353873660
Aleksander Philips <aleksander.philips@gmail.com>
2019-03-01 09:57:27 +0100

    removed rendundant typing

--- tests/object_relations/conftest.py
+++ tests/object_relations/conftest.py
@@ -75,10 +75,8 @@ def one_orm():
 
 # Models
 class IHouse:
-    person: OneRelation
-
     def __init__(self):
-        self.person = OneRelation(to_type=Person)
+        self.person: OneRelation = OneRelation(to_type=Person)
 
 
 class House(IHouse):
@@ -94,40 +92,29 @@ class Cottage(IHouse):
 
 
 class ManyRelationsHouse:
-    person_a: OneRelation
-    person_b: OneRelation
-
     def __init__(self):
-        self.person_a = OneRelation(to_type=Person)
-        self.person_b = OneRelation(to_type=Person)
+        self.person_a: OneRelation = OneRelation(to_type=Person)
+        self.person_b: OneRelation = OneRelation(to_type=Person)
 
 
 class SubstitutionHouse:
-    person: OneRelation
-
     def __init__(self):
-        self.person = OneRelation(to_type=Person, substitution=True)
+        self.person: OneRelation = OneRelation(to_type=Person, substitution=True)
 
 
 class Person:
-    houses: ManyRelation
-
     def __init__(self):
-        self.houses = ManyRelation(to_type=IHouse)
+        self.houses: ManyRelation = ManyRelation(to_type=IHouse)
 
 
 class SsnPerson:
-    ssn: OneRelation
-
     def __init__(self):
-        self.ssn = OneRelation(to_type=Ssn)
+        self.ssn: OneRelation = OneRelation(to_type=Ssn)
 
 
 class Ssn:
-    person: OneRelation
-
     def __init__(self):
-        self.person = OneRelation(to_type=Person)
+        self.person: OneRelation = OneRelation(to_type=Person)
 
 
 # typing
